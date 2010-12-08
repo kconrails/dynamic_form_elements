be418bdc070afea17359e6578d5e4204b2e4b94b
Jaime Bellmyer <online@bellmyer.com>
2010-12-07 18:36:16 -0600

    added full_name accessors and updated form to use them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def full_name= name
+    self.first_name, self.last_name = name.split(/\s+/, 2)
+  end
 end
