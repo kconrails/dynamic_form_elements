9cc28e97565202510c97f5a05fca0fcf0825f7fc
Jaime Bellmyer <online@bellmyer.com>
2010-12-07 18:24:54 -0600

    initial app with user scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicFormElements::Application.config.secret_token = '610a1a5edf673629cac667c8b9a4b6a959c81c6c2b3c9b53c2694f23a30f4de4906ad46bf8cf819ec30da371106fc4a93a4c0e4c9cdb7ffdcee1d24f4fd47635'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DynamicFormElements::Application.config.session_store :cookie_store, :key => '_dynamic_form_elements_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# DynamicFormElements::Application.config.session_store :active_record_store
